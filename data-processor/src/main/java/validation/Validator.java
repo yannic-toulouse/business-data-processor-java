90c7d8de18ac40094a4a7410097e3b8115977c2e
Yannic Toulouse <yannic@toulousenet.de>
2026-02-09 15:44:12 +0100

    Refactored CsvProcessor to use custom exceptions; added validateOrderStatus to Validator

--- data-processor/src/main/java/validation/Validator.java
+++ data-processor/src/main/java/validation/Validator.java
@@ -4,21 +4,35 @@
  */
 package validation;
 
+import model.Order;
+
 import java.util.Locale;
 import java.util.Locale.IsoCountryCode;
 
 public class Validator
 {
-	/**
-	 * Validates that country code is valid ISO3166-1 alpha-2 code
-	 * @param code Country Code
-	 * @return {@code true} if {@code code} is a valid ISO3166-1 alpha-2 country code and returns {@code false} if it is not
-	 */
-	public static boolean validateISOCountryCode(String code)
-	{
-		if(Locale.getISOCountries(IsoCountryCode.PART1_ALPHA2).contains(code))
-			return true;
-		return false;
-	}
-	
+    /**
+     * Validates that country code is valid ISO3166-1 alpha-2 code
+     *
+     * @param code Country Code
+     * @return {@code true} if {@code code} is a valid ISO3166-1 alpha-2 country code and returns {@code false} if it is not
+     */
+    public static boolean validateISOCountryCode(String code)
+    {
+        return Locale.getISOCountries(IsoCountryCode.PART1_ALPHA2).contains(code);
+    }
+
+    public static boolean validateOrderStatus(String status)
+    {
+        try
+        {
+            Order.OrderStatus.valueOf(status);
+            return true;
+        }
+        catch (IllegalArgumentException e)
+        {
+            return false;
+        }
+    }
+
 }
