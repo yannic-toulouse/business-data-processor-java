9a97cd788ef279a886a71911eeb63a10320e6421
Yannic Toulouse <yannic@toulousenet.de>
2026-02-12 12:34:30 +0100

    Added List<Order> attribute to ReportData

--- data-processor/src/main/java/statistics/ReportData.java
+++ data-processor/src/main/java/statistics/ReportData.java
@@ -1,9 +1,13 @@
 package statistics;
 
+import model.Order;
+
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 public record ReportData(
+        List<Order> orderList,
         int orderCount,
         int highValueOrderCount,
         int exportCheckOrderCount,
@@ -14,6 +18,16 @@ public record ReportData(
         Map<String, BigDecimal> orderValueByCountry
 )
 {
+    public ReportData
+    {
+        orderList = List.copyOf(orderList);
+        orderValueByCountry = Map.copyOf(orderValueByCountry);
+    }
+    @Override
+    public List<Order> orderList()
+    {
+        return List.copyOf(orderList);
+    }
 
     @Override
     public BigDecimal avgOrderValue()
