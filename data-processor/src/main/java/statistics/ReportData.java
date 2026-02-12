d3e592f2283d1aa2042fbdb57be1bcd37208d931
Yannic Toulouse <yannic@toulousenet.de>
2026-02-12 17:22:25 +0100

    Added getReportString to ReportData

--- data-processor/src/main/java/statistics/ReportData.java
+++ data-processor/src/main/java/statistics/ReportData.java
@@ -3,6 +3,7 @@ package statistics;
 import model.Order;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -75,5 +76,24 @@ public record ReportData(
     public Map<String, BigDecimal> orderValueByCountry()
     {
         return orderValueByCountry;
+    }
+
+    public String getReportString(NumberFormat nf)
+    {
+        StringBuilder contentToWrite = new StringBuilder();
+        StringBuilder countryValueSB = new StringBuilder();
+
+        contentToWrite.append("Order count = ").append(String.format("%,d", this.orderCount)).append("\n");
+        contentToWrite.append("High value order count = ").append(String.format("%,d", this.highValueOrderCount)).append("\n");
+        contentToWrite.append("Export check order count = ").append(String.format("%,d", this.exportCheckOrderCount)).append("\n");
+        contentToWrite.append("Total order value = ").append(String.format("%s", nf.format(this.totalOrderValue))).append("\n");
+        contentToWrite.append("Average order value = ").append(String.format("%s", nf.format(this.avgOrderValue))).append("\n");
+        contentToWrite.append("Lowest order value = ").append(String.format("%s", nf.format(this.lowestOrderValue))).append("\n");
+        contentToWrite.append("Highest order value = ").append(String.format("%s", nf.format(this.highestOrderValue))).append("\n");
+        contentToWrite.append("Order value by country:").append("\n");
+        for (String country : this.orderValueByCountry.keySet())
+            countryValueSB.append(String.format("%2s: %s", country, nf.format(this.orderValueByCountry().get(country)))).append("\n");
+        contentToWrite.append(countryValueSB);
+        return contentToWrite.toString();
     }
 }
