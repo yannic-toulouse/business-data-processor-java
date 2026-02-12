451ae51e828fc01fa3b688c2c3764815fe86b4b1
Yannic Toulouse <yannic@toulousenet.de>
2026-02-12 17:40:53 +0100

    ReportWriter now uses ReportData.getReportString

--- data-processor/src/main/java/util/ReportWriter.java
+++ data-processor/src/main/java/util/ReportWriter.java
@@ -12,33 +12,19 @@ import java.util.List;
 
 public class ReportWriter
 {
-    public static void writeReportToTxt(List<Order> orderList, File outFile, boolean overwrite) throws IOException
+    public static void writeReportToTxt(ReportData reportData, File outFile, boolean overwrite) throws IOException
     {
         NumberFormat nf = NumberFormat.getCurrencyInstance();
         nf.setMinimumFractionDigits(2);
         nf.setMaximumFractionDigits(2);
-        ReportData reportData = StatisticsProcessor.fullReport(orderList);
-        StringBuilder contentToWrite = new StringBuilder();
-        StringBuilder countryValueSB = new StringBuilder();
-
-        contentToWrite.append("Order count = ").append(String.format("%,d", reportData.orderCount())).append("\n");
-        contentToWrite.append("High value order count = ").append(String.format("%,d", reportData.highValueOrderCount())).append("\n");
-        contentToWrite.append("Export check order count = ").append(String.format("%,d", reportData.exportCheckOrderCount())).append("\n");
-        contentToWrite.append("Total order value = ").append(String.format("%s", nf.format(reportData.totalOrderValue()))).append("\n");
-        contentToWrite.append("Average order value = ").append(String.format("%s", nf.format(reportData.avgOrderValue()))).append("\n");
-        contentToWrite.append("Lowest order value = ").append(String.format("%s", nf.format(reportData.lowestOrderValue()))).append("\n");
-        contentToWrite.append("Highest order value = ").append(String.format("%s", nf.format(reportData.highestOrderValue()))).append("\n");
-        contentToWrite.append("Order value by country:").append("\n");
-        for (String country : reportData.orderValueByCountry().keySet())
-            countryValueSB.append(String.format("%2s: %s", country, nf.format(reportData.orderValueByCountry().get(country)))).append("\n");
-        contentToWrite.append(countryValueSB);
+        String contentToWrite = reportData.getReportString(nf);
 
         try
         {
             if (outFile.createNewFile() || overwrite)
             {
                 System.out.println("[Info] Output file successfully created.");
-                writeContent(outFile, contentToWrite.toString());
+                writeContent(outFile, contentToWrite);
             }
             else
             {
